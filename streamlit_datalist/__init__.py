f668ffb3577b9055212d3d7f20de4bf66957a3b3
RobertoFriasN <robertofnerio@gmail.com>
2022-11-08 17:50:37 -0600

    Update code used during development

--- streamlit_datalist/__init__.py
+++ streamlit_datalist/__init__.py
@@ -1,6 +1,7 @@
 import streamlit.components.v1 as components
 import streamlit as st
 import os
+import pandas as pd
 
 _RELEASE = True
 
@@ -24,13 +25,20 @@ def stDatalist(label:str, options:list, index:int=0, key=None):
     return _streamlit_datalist(label=label, options=options, index=index, key=key, default=None)
 
 
+## Code used during testing and development.
 if not _RELEASE:
- 
-    my_sel = stDatalist('This datalist is...', ["great", "cool", "neat"], key='1')
+    
+    data = {'data':[12345, 67890, 98765]}
 
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('The value you selected is: ', my_sel)
+    my_df = pd.DataFrame(data=data)
+
+    data=["great", "cool", "neat"]
+
+    cont1 = st.container()
+    st.markdown('<hr style="border:0px; background-color:orange; height:2px;">', unsafe_allow_html=True)
+
+    my_sel = stDatalist('This datalist is...', data, key='1')
+    # my_sel = stDatalist('This datalist is...', my_df['data'].tolist(), key='1')
+
+    with cont1:
+        st.write('The value you selected is: ', my_sel)
